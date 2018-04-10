30106f9311284e65436f76434d412fc3f205c49e
tomboyanushka <32500051+tomboyanushka@users.noreply.github.com>
2018-04-10 18:52:04 -0400

    level commit

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private GameObject player;
+    public float cameraSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        Vector3 camPos = transform.position;
+        camPos.x = player.transform.position.x - -9.0f;
+        transform.position = Vector3.Lerp(transform.position, camPos, 3 * Time.fixedDeltaTime);
+
+        //Y pos forward
+        camPos.y = player.transform.position.y + 2;
+        transform.position = Vector3.Lerp(transform.position, camPos, 7.0f * Time.fixedDeltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static float jetpackFuel = 1.5f;
+    public float jetpackForce = 10.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButton("Jump") && jetpackFuel>=0.001f)
+        {
+            BoostUp();
+        }
+	}
+
+    void BoostUp()
+    {
+        jetpackFuel = Mathf.MoveTowards(jetpackFuel, 0, Time.fixedDeltaTime);
+        GetComponent<Rigidbody>().AddForce(new Vector3(0, jetpackForce));
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            jetpackFuel = 1.5f;
+        }
+    }
+}
